6fe1e4c654177ec0c317708f78eda84be0db1735
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-21 17:11:19 -0300

    aula 45 completa

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumero(10, 19);
+    }
+}
