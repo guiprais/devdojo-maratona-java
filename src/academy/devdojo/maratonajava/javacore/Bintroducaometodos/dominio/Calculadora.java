54bd022456885cec9399dfb4dbc5935b1077c2cf
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-21 18:28:29 -0300

    aula 46 completa

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -12,4 +12,11 @@ public class Calculadora {
     public void multiplicaDoisNumero(int num1, int num2) {
         System.out.println(num1 * num2);
     }
+
+    public double divideDoisNumero(double num1, double num2) {
+        if (num2 == 0) {
+            return 0;
+        }
+        return num1 / num2;
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        double result = calculadora.divideDoisNumero(4, 2);
+        System.out.println(result);
+
+    }
+}
