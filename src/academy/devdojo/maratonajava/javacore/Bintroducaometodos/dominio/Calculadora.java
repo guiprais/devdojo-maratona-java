b31a97e9a40993b7504e8287744a0d5f199d5dc8
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-21 23:45:08 -0300

    aula 47 completa

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -19,4 +19,19 @@ public class Calculadora {
         }
         return num1 / num2;
     }
+
+    public double divideDoisNumero02(double num1, double num2) {
+        if (num2 != 0) {
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Não existe divisão por zero");
+            return;
+        }
+        System.out.println(num1 / num2);
+    }
 }
--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest03.java
@@ -5,9 +5,10 @@ import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculad
 public class CalculadoraTest03 {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-
         double result = calculadora.divideDoisNumero(4, 2);
         System.out.println(result);
+        
+        calculadora.imprimeDivisaoDeDoisNumeros(86, 0);
 
     }
 }
