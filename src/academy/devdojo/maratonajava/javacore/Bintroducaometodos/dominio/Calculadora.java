b25455903f61c8744020f278cc0507ccc11c6de3
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-21 23:54:01 -0300

    aula 48 completa

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -34,4 +34,12 @@ public class Calculadora {
         }
         System.out.println(num1 / num2);
     }
+
+    public void alteraDoisNumeros(int num1, int num2) {
+        num1 = 99;
+        num2 = 33;
+        System.out.println("Dentro do alteraDoisNumero");
+        System.out.println("Num1 " + num1);
+        System.out.println("Num2 " + num2);
+    }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest04.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest04 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int num1 =1;
+        int num2 = 2;
+        calculadora.alteraDoisNumeros(num1, num2);
+    }
+}
